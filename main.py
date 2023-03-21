3e8d95a12d6f2bfe94c7465f54f14e1671eafa1c
vanguardyy <79261421+vanguardyy@users.noreply.github.com>
2023-03-21 23:03:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -184,9 +184,9 @@ def send_message(to_user, access_token, region_name,weather, temp, tempn, wind_d
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
         if birth_day == 0:
-            birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
+            birthday_data = "今天回国啦！".format(value["name"], value["name"])
         else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+            birthday_data = "距离修阔爱回国还有{}天".format(birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
